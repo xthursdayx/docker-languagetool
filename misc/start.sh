0b220eb1ffaf29fab40dd3321fc60f589cc0207d
thursday <xthursdayx@mailbox.org>
2021-08-04 22:28:13 +0000

    Update start.sh
    Add yasu

--- misc/start.sh
+++ misc/start.sh
@@ -3,4 +3,10 @@
 EXTRAOPTIONS=""
 [ -d "/ngrams" ] && EXTRAOPTIONS=" --languageModel /ngrams "
 
-java -cp languagetool-server.jar  org.languagetool.server.HTTPServer --port 8010 --public --allow-origin '*' ${EXTRAOPTIONS}
+# Check for English spell checker improvments
+# http://wiki.languagetool.org/hunspell-support
+if [ -f /spelling/en_spelling_additions.txt ]; then
+	echo; cat en_spelling_additions.txt >> org/languagetool/resource/en/hunspell/spelling_custom.txt
+fi
+
+yasu java -cp languagetool-server.jar  org.languagetool.server.HTTPServer --port 8010 --public --allow-origin '*' ${EXTRAOPTIONS}
